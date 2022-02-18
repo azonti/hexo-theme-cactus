bef0cc6797b9af05263581ca01233e8767e0f060
Shu Takayama <syu.takayama@gmail.com>
2022-02-18 17:57:23 +0900

    profile layout updated

--- scripts/page_title.js
+++ scripts/page_title.js
@@ -5,7 +5,7 @@
  *     <%- page_title() %>
  */
 hexo.extend.helper.register("page_title", function () {
-  var title = this.page.title ? this.page.title : this.config.title;
+  var title = this.page.title ? this.page.title + ' - ' + this.config.title : this.config.title;
 
   if (this.is_archive()) {
     title = this.__("nav.articles");
@@ -15,10 +15,16 @@ hexo.extend.helper.register("page_title", function () {
     } else if (this.is_year()) {
       title += ": " + this.page.year;
     }
+
+    title += ' - ' + this.config.title;
   } else if (this.is_category()) {
     title = this.__("nav.category") + ": " + this.page.category;
+
+    title += ' - ' + this.config.title;
   } else if (this.is_tag()) {
     title = this.__("nav.tag") + ": " + this.page.tag;
+
+    title += ' - ' + this.config.title;
   }
 
   return title;
